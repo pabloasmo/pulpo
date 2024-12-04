5a7c198e3e7f1c2b8e09d43f24cfa4ca6b07ddeb
pabloasmo <pablo.astorga.mora.alu@iesfernandoaguilar.es>
2024-12-04 18:11:47 +0100

    fix error

--- pulpo_project/pulpo_project/urls.py
+++ pulpo_project/pulpo_project/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from pulpo_app.views import 
-
 from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
