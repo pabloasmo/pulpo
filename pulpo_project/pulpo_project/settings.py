96ed61cae6ee7e1d153e9c95ac16bfac7e9478c3
pabloasmo <pablo.astorga.mora.alu@iesfernandoaguilar.es>
2024-12-04 18:53:15 +0100

    add python-dotenv

--- pulpo_project/pulpo_project/settings.py
+++ pulpo_project/pulpo_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,4 +143,3 @@ REST_FRAMEWORK = {
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
